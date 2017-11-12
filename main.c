e78e91056449952c411a7f16a5df4a3d59555bce
thomas <thomas@Bookie.geenbs>
2017-11-12 23:34:34 +0100

    Cleaned up usage of freelinkedlist function

--- main.c
+++ main.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "generic_unix_tools.h"
 
+typedef struct chanList{
+    char chanName[100];
+    struct chanList *next;
+}chanList;
+
+void addChannel(chanList *chans, char *chanName, int len);
+
 int main(void){
+    //Inititalize and populate linked list
+    chanList *chans = malloc(sizeof(chanList));
+    addChannel(chans, "test", 4);
+    addChannel(chans, "kest", 4);
+    addChannel(chans, "rest", 4);
 
-    typedef struct agenericLinkedList{
-        int a;
-        char b;
-        struct agenericLinkedList *next;
-    }aL;
+    freeLinkedListGen((void *)chans); //Explicitly casting not really needed
 
-    //Start list
-    aL *myList = malloc(sizeof(aL));
-    myList->a = 10;
-    myList->b = 'c';
-    myList->next = NULL;
-
-    //add another
-    myList->next = malloc(sizeof(aL));
-    myList->next->a = 11;
-    myList->next->b = 'd';
-    myList->next->next = NULL;
-    
-
-    //freeLinkedListGen(myList);
-
-    //freeLinkedListGen((void *)myList);
-    freeLinkedListGen(myList);
     return 0;
+}
+
+//Adds node to linked list
+void addChannel(chanList *chans, char *chanName, int len){
+    chanList *head = chans;
+    while(head->next != NULL){
+        head = head->next;
+    }
+    
+    head->next = malloc(sizeof(chanList));
+    memcpy(head->next->chanName, chanName, len); 
+    head->next->chanName[len] = '\0';
+    head->next->next = NULL;
 }
