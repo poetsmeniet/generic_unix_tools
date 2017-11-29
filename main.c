3f770d3e00ce4a58ffe72642ef42a2e2a992a093
Thomas <thomas@geenbs.nl>
2017-11-29 10:54:51 +0100

    Implemented example of SIGINT catcher

--- main.c
+++ main.c
@@ -1,36 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include "generic_unix_tools.h"
 
-typedef struct chanList{
-    char chanName[100];
-    struct chanList *next;
-}chanList;
-
-void addChannel(chanList *chans, char *chanName, int len);
-
-int main(void){
-    //Inititalize and populate linked list
-    chanList *chans = malloc(sizeof(chanList));
-    addChannel(chans, "test", 4);
-    addChannel(chans, "kest", 4);
-    addChannel(chans, "rest", 4);
-
-    freeLinkedListGen((void *)chans); //Explicitly casting not really needed
-
-    return 0;
+void myFunc()
+{
+    printf("Signal processed\n");
 }
 
-//Adds node to linked list
-void addChannel(chanList *chans, char *chanName, int len){
-    chanList *head = chans;
-    while(head->next != NULL){
-        head = head->next;
-    }
+int main(void)
+{
     
-    head->next = malloc(sizeof(chanList));
-    memcpy(head->next->chanName, chanName, len); 
-    head->next->chanName[len] = '\0';
-    head->next->next = NULL;
+    //Demonstrate catch CTR-C event
+    catchSIGINT(&myFunc);
+
+    printf("Waiting for signal..\n");
+    sleep(10);
+
+    return 0;
 }
